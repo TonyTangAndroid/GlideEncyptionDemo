2936c4bb6adf4201348091d9a92bcf2a9931293a
ztang <tonythompsoncmu@gmail.com>
2018-12-10 17:23:38 -0500

    Fix aar loading

--- okhttp3_lib/src/main/java/com/github/tonytanganadroid/glide/okhttp3/lib/YourOkhttpAppGlideModule.java
+++ okhttp3_lib/src/main/java/com/github/tonytanganadroid/glide/okhttp3/lib/YourOkhttpAppGlideModule.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.Excludes;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
 import com.bumptech.glide.load.model.GlideUrl;
@@ -16,8 +17,8 @@ import java.util.concurrent.TimeUnit;
 import okhttp3.OkHttpClient;
 
 @GlideModule
+@Excludes({com.bumptech.glide.integration.okhttp3.OkHttpLibraryGlideModule.class})
 public class YourOkhttpAppGlideModule extends AppGlideModule {
-
 
     public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
         OkHttpUrlLoader.Factory factory = new OkHttpUrlLoader.Factory(client());
@@ -31,5 +32,8 @@ public class YourOkhttpAppGlideModule extends AppGlideModule {
                 .build();
     }
 
-
+    @Override
+    public boolean isManifestParsingEnabled() {
+        return false;
+    }
 }
