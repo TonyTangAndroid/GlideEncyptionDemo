457c63fd0ffe3670a3e66ad8f6471a2be44cb32c
ztang <tonythompsoncmu@gmail.com>
2018-12-10 19:08:53 -0500

    Add test script

--- it.sh
+++ it.sh
@@ -5,4 +5,5 @@ gradle clean
 gradle assembleRelease
 cp okhttp3_lib/build/outputs/aar/okhttp3_lib-release.aar aars/glide.aar
 unzip aars/glide.aar -d temp
+unzip temp/classes.jar -d temp/classes
 cp settings.gradle.bk settings.gradle
